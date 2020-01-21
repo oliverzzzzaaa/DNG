e19e6d9a3a69269a1bce77f1cff0300273cfe79b
Guanyao Wang <guw005@eng.ucsd.edu>
2020-01-21 10:02:25 -0800

    profile icon

---
+++ client/src/components/profile/profile_icon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./profile_icon.css";
+
+class ProfileIcon extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        
+    }
+}
+        
+
+export default ProfileIcon;
