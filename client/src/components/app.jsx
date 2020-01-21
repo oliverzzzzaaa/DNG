be91dc522c7d1e1aff169093a63416f9a54e5a33
Oliver <oliverzzzzaaa@gmail.com>
2020-01-21 09:26:20 -0800

    new app

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import Lobby from "./main/lobby";
 import { Route } from "react-router-dom";
-<<<<<<< HEAD
 import ClientComponentExample from "./clientComponentExample";
 import Client2 from "./clientExample2";
 
@@ -14,6 +13,6 @@ const App = () => (
       <Route exact path="/" component={Lobby} />
     </Switch>
   </div>
-
+);
 
 export default App;
