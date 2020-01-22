38b0cd34d25fae11fb02a3585567ae302c16ad83
KJW <johnson9710@gmail.com>
2020-01-22 14:02:32 -0800

    home page carousel

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -7,13 +7,15 @@ import Client2 from "./clientExample2";
 import SignupForm from './session/signup_form_container';
 import LoginForm from './session/login_form_container';
 import { AuthRoute, ProtectedRoute} from '../util/route';
+import HomePage from './home';
 
 
 const App = () => (
   <div>
     <Switch>
-      <AuthRoute exact path="/" component={LoginForm} />
+      <AuthRoute exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
+      <Route exact path='/home' component={HomePage} /> 
       <ProtectedRoute exact path="/lobby" component={Lobby} />
     </Switch>
   </div>
