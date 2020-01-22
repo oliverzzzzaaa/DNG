152b67db981c4a300c7375261f9e172c7fe035d5
Yinqian Zheng <yinqianzheng@gmail.com>
2020-01-21 18:18:22 -0800

    add structure standard for data

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import Lobby from "./main/lobby";
 import { Route } from "react-router-dom";
-import ClientComponentExample from "./clientComponentExample";
-import Client2 from "./clientExample2";
-import SignupForm from './session/signup_form_container';
-import LoginForm from './session/login_form_container';
+import SignupForm from "./session/signup_form_container";
+import LoginForm from "./session/login_form_container";
 
 const App = () => (
   <div>
-    <ClientComponentExample />
-    <Client2 />
     <Switch>
-      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
-      <Route exact path="/" component={Lobby} />
+      <Route exact path="/lobby" component={Lobby} />
     </Switch>
   </div>
 );
