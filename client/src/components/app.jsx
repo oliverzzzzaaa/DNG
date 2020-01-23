a90a45a53b702f15e4986076f5553a1e10537f74
Yinqian Zheng <yinqianzheng@gmail.com>
2020-01-22 16:45:42 -0800

    add modal for join room

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { Switch } from "react-router-dom";
 import LobbyContainer from "./main/lobbyContainer";
-import { Route } from "react-router-dom";
 import ClientComponentExample from "./clientComponentExample";
 import Client2 from "./clientExample2";
-
+import HomePage from "./home";
 import Room from "./room/room";
 import SignupForm from "./session/signup_form_container";
 import LoginForm from "./session/login_form_container";
 import { AuthRoute, ProtectedRoute } from "../util/route";
 
-
 const App = () => (
   <div>
     <Switch>
-      <AuthRoute exact path='/' component={HomePage} /> 
+      <AuthRoute exact path="/" component={HomePage} />
       <AuthRoute exact path="/login" component={LoginForm} />
       <AuthRoute exact path="/signup" component={SignupForm} />
       <ProtectedRoute exact path="/lobby" component={LobbyContainer} />
