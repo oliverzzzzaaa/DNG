b5598feea8c9f66c75c4b03932df207c8f3c4294
Oliver <oliverzzzzaaa@gmail.com>
2020-01-26 23:24:59 -0800

    changed server to client build

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/public'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  })
-}
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+  }
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
