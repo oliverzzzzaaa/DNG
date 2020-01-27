bca26815a77d9fab464082b2743d743177dd2fea
Oliver <oliverzzzzaaa@gmail.com>
2020-01-26 18:26:06 -0800

    frontend build

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose
   .catch(err => console.log(err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/public'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   })
 }
 
