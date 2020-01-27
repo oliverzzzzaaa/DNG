2f1084470fd90e8e42e1b077cbc0dc8071cf37e0
Yinqian Zheng <yinqianzheng@gmail.com>
2020-01-26 21:54:07 -0800

    add procfile for heroku

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    path.resolve(__dirname, "client", "build", "index.html");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
