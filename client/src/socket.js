3aff6df324e8d0382421a3129d2144228051eb0e
Yinqian Zheng <yinqianzheng@gmail.com>
2020-02-03 09:46:18 -0800

    change endpoint for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,11 @@ export default class MySocket {
   socket;
   static getSocket = userId => {
     if (MySocket.socket === undefined) {
-      const endpoint = "http://localhost:5000";
+      const port = process.env.PORT || 5000;
+      let endpoint = `http://localhost:${port}`;
+      if (process.env.NODE_ENV === "production") {
+        endpoint = `http://pictionary4.herokuapp.com:${port}`;
+      }
       MySocket.socket = openSocket(endpoint);
       if (userId) {
         MySocket.socket.emit("login", { userId: userId });
