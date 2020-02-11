8d710555d59d50dd369b66a9d4ae07266a843f7f
Yinqian Zheng <yinqianzheng@gmail.com>
2020-02-10 17:29:42 -0800

    add prodution middleware

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,9 +3,12 @@ import logger from "redux-logger";
 import rootReducer from "../reducers/root";
 import thunk from "redux-thunk";
 
-
+const middlewares = [thunk];
+if (process.env.NODE_ENV === "production") {
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 
 export default configureStore;
