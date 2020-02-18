b135a10bb9f256a131373856f219687a6b42cf71
KJW <johnson9710@gmail.com>
2020-02-17 18:02:26 -0800

    join room modal CSS

--- client/src/components/games/game_rooms/modal.jsx
+++ client/src/components/games/game_rooms/modal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import './modal.css';
 
 export default class JoinModal extends React.Component {
   constructor(props) {
